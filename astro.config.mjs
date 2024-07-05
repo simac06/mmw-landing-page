73c9dd8adad2f839889fae93e678f7b3e77a4586
simac06 <75936175+simac06@users.noreply.github.com>
2024-07-04 22:35:24 -0400

    Updated favicon and fixed env variable issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 import sanity from "@sanity/astro";
+import { loadEnv } from "vite";
 
 import vercel from "@astrojs/vercel/serverless";
-const SANITY_PROJECT_ID = import.meta.env.SANITY_PROJECT_ID;
-const SANITY_DATASET = import.meta.env.SANITY_DATASET;
-const SANITY_API_VER = import.meta.env.SANITY_API_VER;
+
+const env = loadEnv("", process.cwd(), "SANITY");
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,13 +16,13 @@ export default defineConfig({
     react(),
     icon(),
     sanity({
-      projectId: SANITY_PROJECT_ID,
-      dataset: SANITY_DATASET,
-      apiVersion: SANITY_API_VER,
+      projectId: env.SANITY_PROJECT_ID,
+      dataset: env.SANITY_DATASET,
+      apiVersion: env.SANITY_API_VER,
       useCdn: true,
     }),
   ],
   output: "server",
   adapter: vercel(),
-  site: "https://musicsmostwanted.com",
+  site: "https://mmw-landing-page.vercel.app",
 });
